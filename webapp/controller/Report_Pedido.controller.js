211b4d3ad107edd42876c727ca322dcc58c9866f
FabianFxJ26 <elkin3001@gmail.com>
2025-06-09 23:00:25 +0000

    That change calculate the total effective amount and execute the navigation until the SAP APP Estandar

--- webapp/controller/Report_Pedido.controller.js
+++ webapp/controller/Report_Pedido.controller.js
@@ -2,32 +2,32 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/export/Spreadsheet",
-    "sap/ui/export/library"
-
-], (Controller, Filter, FilterOperato,Spreadsheet,exportLibrary) => {
+    "sap/ui/export/Spreadsheet"
+], (Controller, Filter, FilterOperator, Spreadsheet) => {
     "use strict";
 
     return Controller.extend("conconcentro.purchases.controller.Report_Pedido", {
         onInit() {
-            var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/YY1_REPORTE_ENTRADAS_PEDID_CDS/", {
+            const oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/YY1_REPORTE_ENTRADAS_PEDID_CDS/", {
                 json: true,
                 useBatch: false
-              });
-              this.getView().setModel(oModel);
+            });
+            this.getView().setModel(oModel);
         },
-        onSearch: function () {
-            const oView = this.getView();
 
+        onSearch() {
+            const oView = this.getView();
             const sProveedor = oView.byId("searchField").getValue();
             const sPedido = oView.byId("inputPedido").getValue();
             const sElementoPEP = oView.byId("inputElementoPEP").getValue();
 
             const aFilters = [];
-
+            const aSupplierOnlyFilters = [];
 
             if (sProveedor) {
-                aFilters.push(new Filter("Supplier", FilterOperator.Contains, sProveedor));
+                const oFilterSupplier = new Filter("Supplier", FilterOperator.Contains, sProveedor);
+                aFilters.push(oFilterSupplier);
+                aSupplierOnlyFilters.push(oFilterSupplier);
             }
             if (sPedido) {
                 aFilters.push(new Filter("PurchaseOrder", FilterOperator.Contains, sPedido));
@@ -35,55 +35,68 @@ sap.ui.define([
             if (sElementoPEP) {
                 aFilters.push(new Filter("WBSElementExternalID", FilterOperator.Contains, sElementoPEP));
             }
+
             const oTable = oView.byId("reportTable");
             const oBinding = oTable.getBinding("items");
             oBinding.filter(aFilters);
+
+            this._calcularTotal(aFilters, "/YY1_REPORTE_ENTRADAS_PEDID", "EffectiveAmount", "effectiveAmountPurchaseOrder");
+            this._calcularTotal(aSupplierOnlyFilters, "/YY1_REPORTE_ENTRADAS_PEDID", "EffectiveAmount", "effectiveAmountSupplier");
         },
-        onClearFilters: function () {
-            const oView = this.getView();
-      
-         
-            oView.byId("searchField").setValue("");
-            oView.byId("inputPedido").setValue("");
-            oView.byId("inputElementoPEP").setValue("");
-      
-           
-            const oTable = oView.byId("reportTable");
-            const oBinding = oTable.getBinding("items");
-            oBinding.filter([]);
-          },
-          onClearFilters: function () {
-            const oView = this.getView();
-            oView.byId("searchField").setValue("");
-            oView.byId("inputPedido").setValue("");
-            oView.byId("inputElementoPEP").setValue("");
 
-            const oTable = oView.byId("reportTable");
-            const oBinding = oTable.getBinding("items");
-            oBinding.filter([]);
-        },
-        onExport: function () {
-            const oTable = this.getView().byId("reportTable");
-            const oBinding = oTable.getBinding("items");
+        _calcularTotal(aFilters, sPath, sField, sControlId) {
+            const oModel = this.getView().getModel();
+            if (!oModel) return;
 
-            const aCols = this._createColumnConfig();
-
-            const oExportSettings = {
-                workbook: {
-                    columns: aCols
+            oModel.read(sPath, {
+                filters: aFilters,
+                success: (oData) => {
+                    const fTotal = oData.results.reduce((sum, item) => sum + (parseFloat(item[sField]) || 0), 0);
+                    this.getView().byId(sControlId).setText(fTotal.toFixed(2));
                 },
-                dataSource: oBinding.getContexts(0, oBinding.getLength()).map(oContext => oContext.getObject()),
-                fileName: "ReporteEntradasPedidos.xlsx",
-                worker: false // true si deseas usar un web worker (recomendado en grandes volúmenes)
-            };
-
-            const oSpreadsheet = new Spreadsheet(oExportSettings);
-            oSpreadsheet.build().finally(() => {
-                oSpreadsheet.destroy();
+                error: () => {
+                    this.getView().byId(sControlId).setText("0.00");
+                }
             });
         },
 
-        _createColumnConfig: function () {
+        onClearFilters() {
+            const oView = this.getView();
+            oView.byId("searchField").setValue("");
+            oView.byId("inputPedido").setValue("");
+            oView.byId("inputElementoPEP").setValue("");
+
+            const oTable = oView.byId("reportTable");
+            const oBinding = oTable.getBinding("items");
+            oBinding.filter([]);
+
+            oView.byId("effectiveAmountPurchaseOrder").setText("0.00");
+            oView.byId("effectiveAmountSupplier").setText("0.00");
+        },
+
+        onExport() {
+            const oModel = this.getView().getModel();
+            if (!oModel) return;
+
+            oModel.read("/YY1_REPORTE_ENTRADAS_PEDID", {
+                success: (oData) => {
+                    const aCols = this._createColumnConfig();
+                    const oExportSettings = {
+                        workbook: { columns: aCols },
+                        dataSource: oData.results,
+                        fileName: "ReporteEntradasPedidos.xlsx",
+                        worker: false
+                    };
+                    const oSpreadsheet = new Spreadsheet(oExportSettings);
+                    oSpreadsheet.build().finally(() => oSpreadsheet.destroy());
+                },
+                error: () => {
+                    sap.m.MessageToast.show("Error al cargar datos para exportar");
+                }
+            });
+        },
+
+        _createColumnConfig() {
             return [
                 { label: "Pedido", property: "PurchaseOrder" },
                 { label: "Fecha Clave", property: "CreationDate", type: "date" },
@@ -97,6 +110,12 @@ sap.ui.define([
                 { label: "Valor entregado con IVA", property: "ValorEntregado", type: "number" },
                 { label: "Cantidad del pedido", property: "OrderQuantity", type: "number" }
             ];
+        },
+
+        onPedidoPress(oEvent) {
+            const sPurchaseOrder = oEvent.getSource().getText();
+            const sUrl = `https://my418453.s4hana.cloud.sap/ui#PurchaseOrder-manage&/C_PurchaseOrderTP(PurchaseOrder='${encodeURIComponent(sPurchaseOrder)}',DraftUUID=guid'00000000-0000-0000-0000-000000000000',IsActiveEntity=true)/?sap-iapp-state--history=TASZ5AJSLBXW5GY8TJKW74FYJNRXTJLMQRWNSSDS3O`;
+            window.open(sUrl, "_blank");
         }
     });
 });
